294b9608a103efd5d4a6a8dc6b3b73fd6bae0038
TheAlphadog96 <bert_1996@hotmail.com>
2015-05-29 18:38:20 +0000

    alles toegevoegd

---
+++ js/map.js
@@ -0,0 +1,56 @@
+jQuery(function($){
+
+	var longitude = 51.143910;
+	var latitude = 4.548013;
+	var canvas = "map";
+
+	
+	function randing_map(canvas, lan, lat){			
+			var myLatlng = new google.maps.LatLng(lan,lat);
+			var myOptions = {
+						zoom: 13,
+						center: myLatlng,
+						mapTypeId: google.maps.MapTypeId.ROADMAP,
+						maxZoom   : 20,
+    					disableDefaultUI: true
+					}			
+			var map = new google.maps.Map( document.getElementById(canvas), myOptions );
+			var marker = new google.maps.Marker({
+			    position : myLatlng,
+			    map      : map,
+			    icon     : "img/point.png"
+			});
+			var styles = [
+			  {
+			    featureType: "all",
+			    stylers: [
+			      { saturation: -80 }
+			    ]
+			  },{
+			    featureType: "road.arterial",
+			    elementType: "geometry",
+			    stylers: [
+			      { hue: "#00ffee" },
+			      { saturation: 50 }
+			    ]
+			  },{
+			    featureType: "poi.business",
+			    elementType: "labels",
+			    stylers: [
+			      { visibility: "off" }
+			    ]
+			  }
+			];	
+			var infowindow = new google.maps.InfoWindow({
+				content:"<div class='map_adresse'><div class='map_address'><span class='address'>Adres : </span>Plaslaar 15, 2500 Lier België</div> <div class='map_tel'><span class='tel'>Gsm : </span>0493 04 94 00</div></div>"
+			});	
+			
+			map.setOptions({styles: styles});
+
+			google.maps.event.addListener(marker, 'click', function() {
+			  infowindow.open(map,marker);
+			});
+	}
+	randing_map(canvas, longitude, latitude);
+
+});
